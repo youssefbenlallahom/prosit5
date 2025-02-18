c3740b1869377e55fb275bd1cf5f22b90f1f55f9
youssefbello <youssef.benlallahom@esprit.tn>
2025-02-18 16:27:31 +0100

    commited successfully

--- src/tn/esprit/gestionzoo/main/ZooManagement.java
+++ src/tn/esprit/gestionzoo/main/ZooManagement.java
@@ -1,10 +1,7 @@
 package tn.esprit.gestionzoo.main;
 
-import tn.esprit.gestionzoo.entities.Animal;
-import tn.esprit.gestionzoo.entities.Dolphin;
-import tn.esprit.gestionzoo.entities.Penguin;
-import tn.esprit.gestionzoo.entities.Terrestrial;
-import tn.esprit.gestionzoo.entities.Zoo;
+import tn.esprit.gestionzoo.entities.*;
+
 class ZooManagement {
     public static void main(String[] args) {
         Animal lion=new Animal("El branda9","lion",15,true);
@@ -15,9 +12,11 @@ class ZooManagement {
         //myZoo.display();
         Dolphin flipper = new Dolphin("Flipper","Flipper", 5, true, "Ocean", 10.5f);
         Penguin pingu = new Penguin("Pingu","Pingu", 3, false, "Arctic", 20.0f);
-
+        Aquatic Aquatic1= new Aquatic("fish","Nemo",2,false,"Ocean");
         System.out.println(flipper);
         System.out.println(pingu);
+        Aquatic1.swim();
+
 
     }
 }
